252cc237db1cdfa609783107d9e4b8e371e0fa4d
frederik <frederik.de.graaf@ing.nl>
2017-11-05 14:07:07 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "env": {
+    "browser": true
+  },
+  "plugins": ["html"],
+  "globals": {
+    "html": false,
+    "render": false,
+    "LionLitElement": false
+  },
+  "rules": {
+    "no-console": ["error", {
+      allow: ["warn", "error"]
+    }],
+    "no-underscore-dangle": [2, {
+      "allowAfterThis": true
+    }],
+
+    "class-methods-use-this": 0,
+  }
+};
