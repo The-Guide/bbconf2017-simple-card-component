1aa2ce1b5e865310e6d4c193613d9d665504419a
frederik <frederik.de.graaf@ing.nl>
2017-11-09 16:22:13 +0100

    created step-5

--- simple-card.js
+++ simple-card.js
@@ -1,7 +1,40 @@
 class SimpleCard extends Bbconf2017LitElement(HTMLElement) {
-  // connectedCallback() {
-  //   console.log('simple-card element added to the DOM!')
-  // }
+  get styleTemplate() {
+    return html`
+     <style>
+        :host {
+          background: #fff;
+          border-radius: 4px;
+          box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+          0 1px 5px 0 rgba(0, 0, 0, 0.12),
+          0 3px 1px -2px rgba(0, 0, 0, 0.2);
+          position: relative;
+          display: block;
+          box-sizing: border-box;
+          line-height: 1.5;
+          font-weight: normal;
+          width: 100%;
+        }
+        #header ::slotted(*), 
+        #main ::slotted(*), 
+        #footer ::slotted(*){
+           padding: 24px 24px;
+        }
+        #header {
+          color: inherit;
+        }
+        #header ::slotted(h1),
+        #header ::slotted(h2),
+        #header ::slotted(h3),
+        #header ::slotted(h4),
+        #header ::slotted(h5),
+        #header ::slotted(h6) {
+          margin-top: 0 !important;
+          margin-bottom: 0 !important;
+        }
+      </style>
+    `;
+  }
 
   get headerTemplate() {
     return html`
@@ -27,6 +60,7 @@ class SimpleCard extends Bbconf2017LitElement(HTMLElement) {
 
   render() {
     return html`
+      ${this.styleTemplate}
       ${this.headerTemplate}
       ${this.bodyTemplate}
       ${this.footerTemplate}
