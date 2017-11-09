626c5ee5f2985a7c37ba41bb8d6c92edd53b3d56
frederik <frederik.de.graaf@ing.nl>
2017-11-09 15:09:26 +0100

    created step-4

--- simple-card.js
+++ simple-card.js
@@ -1,4 +1,23 @@
 class SimpleCard extends Bbconf2017LitElement(HTMLElement) {
+  get headerTemplate() {
+    return html`
+      <header id="header">
+        <slot name="header" id="headerslot"></slot>
+      </header>
+  `}
+  get bodyTemplate() {
+    return html`
+      <main id="main">
+        <slot name="main" id="mainslot"></slot>
+      </main>
+  `}
+  get footerTemplate() {
+    return html`
+      <footer id="footer">
+        <slot name="footer" id="footerslot"></slot>
+      </footer>
+  `}
+
   connectedCallback() {
     console.log('simple-card element added to the DOM!')
   }
