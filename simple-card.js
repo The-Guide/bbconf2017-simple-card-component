836f93dfcd72bddb2537448a642b3bbd574fb517
frederik <frederik.de.graaf@ing.nl>
2017-11-09 11:16:05 +0100

    created step-3

--- simple-card.js
+++ simple-card.js
@@ -1,7 +1,7 @@
-class SimpleCard extends HTMLElement {
- connectedCallback(){
-   console.log('simple-card element added to the DOM!')
- }
+class SimpleCard extends Bbconf2017LitElement(HTMLElement) {
+  connectedCallback() {
+    console.log('simple-card element added to the DOM!')
+  }
 }
 
-customElements.define('simple-card',SimpleCard);
+customElements.define('simple-card', SimpleCard);
