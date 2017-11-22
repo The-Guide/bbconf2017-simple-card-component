20f631f3f4405afe043583f140b70fb1dd2513eb
Gerjan van Geest <gerjan.van.geest@ing.nl>
2017-11-22 17:51:37 +0100

    fix(chore): Bring simple-card more in line with the codelab

--- simple-card.js
+++ simple-card.js
@@ -17,32 +17,17 @@ class SimpleCard extends Bbconf2017LitElement(HTMLElement) {
           font-weight: normal;
           width: 100%;
         }
-
-        #header ::slotted(*), #main ::slotted(*) {
-          padding: calc(var(--spacer-vertical, 8px) * 3) calc(var(--spacer-horizontal, 8px) * 3);
+         
+        #header ::slotted(*),
+        #body ::slotted(*),
+        #footer ::slotted(*){
+          padding: 24px;
         }
-
-        #header ::slotted(img), #main ::slotted(img), #footer ::slotted(img) {
-          padding: 0;
-          object-fit: cover;
-          width: 100%;
-          height: 200px;
-          border-bottom: none;
-          display: block;
-        }
-
+        
         #header {
-          color: var(--primary-text-color, inherit);
+          color: inherit;
         }
-
-        #header ::slotted(*:first-of-type) {
-          border-bottom: 1px solid var(--divider-color, #d5d5d5);
-        }
-
-        #header ::slotted(img:first-child) {
-          border-radius: var(--border-radius, 4px) var(--border-radius, 4px) 0 0;
-        }
-
+        
         #header ::slotted(h1),
         #header ::slotted(h2),
         #header ::slotted(h3),
@@ -51,15 +36,6 @@ class SimpleCard extends Bbconf2017LitElement(HTMLElement) {
         #header ::slotted(h6) {
           margin-top: 0 !important;
           margin-bottom: 0 !important;
-        }
-
-        #footer ::slotted(*:first-of-type) {
-          display: block;
-          border-top: 1px solid var(--divider-color, #d5d5d5);
-        }
-
-        #footer ::slotted(*:last-child) {
-          --lion-bar-border-bottom: none;
         }
       </style>
     `;
@@ -75,9 +51,9 @@ class SimpleCard extends Bbconf2017LitElement(HTMLElement) {
 
   get bodyTemplate() {
     return html`
-      <main id="main">
-        <slot id="mainslot"></slot>
-      </main>
+      <div id="body">
+        <slot id="bodyslot"></slot>
+      </div>
     `;
   }
 
